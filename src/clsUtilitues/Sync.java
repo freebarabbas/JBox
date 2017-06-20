c0143d720120b570aa12d69bbd30a54524ebd52c
chianingwang@hotmail.com <johnny634917>
2017-06-20 16:54:33 -0700

    test git push

--- src/clsUtilitues/Sync.java
+++ src/clsUtilitues/Sync.java
@@ -131,8 +131,6 @@ public class Sync implements Runnable {
 						strmarker = lines[i];
 						counter++;
 				}
-				//System.out.println(counter);
-				//System.out.println(strmarker);
 			}
 			while ( counter !=0 && ( counter % 10000 ) == 0){
 				RestResult rrmore=RestConnector.GetContainer(m_tkn, m_usercontainer + "?marker=" + strmarker, m_pxy);
@@ -146,8 +144,6 @@ public class Sync implements Runnable {
 						    strmarker = lines[i];
 							counter++;
 					}
-					//System.out.println(counter);
-					//System.out.println(strmarker);
 				}
 			}
 		}
