8b4a6e38587147b7494cb4f324c726fe0e0b12fa
chianingwang@hotmail.com <johnny634917>
2017-09-14 06:57:22 -0700

    add Runnable and Callable into git index

---
+++ src/clsFSWatcher/TestSyncCallable.java
@@ -0,0 +1,27 @@
+package clsFSWatcher;
+
+	
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+class CallableExample implements Callable<Object>
+{
+
+    public Object call() throws Exception
+    {
+        // Create random number generator
+        Random generator = new Random();
+
+        Integer randomNumber = generator.nextInt(5);
+
+        // To simulate a heavy computation,
+        // we delay the thread for some random time
+        Thread.sleep(randomNumber * 1000);
+
+        return randomNumber;
+    }
+}
+
+
+
+
